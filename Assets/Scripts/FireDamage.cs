513fe87cab74addf0a3bf526671f534d633e8881
Sudpor <kittipitsudpor@gmail.com>
2023-10-27 02:44:07 +0900

    //update

--- Assets/Scripts/FireDamage.cs
+++ Assets/Scripts/FireDamage.cs
@@ -17,7 +17,7 @@ public class FireDamage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        timer++;
     }
 
     private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
@@ -32,7 +32,7 @@ public class FireDamage : MonoBehaviour
 
     private void OnTriggerStay2D(UnityEngine.Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && timer % 2 < Time.deltaTime)
+        if (collision.gameObject.CompareTag("Player") && timer % 60 >= 55)
         {
             playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
             playerHealth.TakeDamage(collisionDamage);
